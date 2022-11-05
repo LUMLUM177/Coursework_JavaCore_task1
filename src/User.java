690bdff4d80a3e8466c5b999c2fc296ca2426d4e
LUM <lumlum177@gmail.com>
2022-11-05 15:01:58 +0300

    Коммит после исправления замечаний

--- src/User.java
+++ src/User.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class User implements Comparable<User> {
 
     private final String name;
@@ -22,6 +24,19 @@ public class User implements Comparable<User> {
                 "name='" + name + '\'' +
                 ", age=" + age +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return age == user.age && name.equals(user.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
     }
 
     @Override
